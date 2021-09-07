3dfa1ff0dc2d0b4b05b97bdb7ce1e935bec137d3
artsasse <artsasse@gmail.com>
2021-09-07 18:01:55 -0300

    add functions to init and generate processes

--- scheduler.c
+++ scheduler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct process {
     // PCB
@@ -12,7 +13,7 @@ typedef struct process {
     int time_io[6]; /* [tempo_disco, inicio_disco, tempo_fita, inicio_fita, tempo_impressora, inicio_impressora] */
 
     // Ponteiro de fila
-    Process* next;
+    struct process* next;
 } Process;
 
 // Adicionar processo em um fila (ALEXANDRE)
@@ -40,10 +41,60 @@ Process** magnetic_tape_queue;
 
 // Gerar processos de exemplo (LUCAS)
     // Retornar array de ponteiros de processos
-Process** generate_processes();
+
+//Inicializa as informações de cada processo
+void init_process(Process* p, int pid, int status, int priority, int time_cpu, int arrival, int time_io[]) {
+    p->pid = pid;
+    p->status = status;
+    p->priority = priority;
+    p->time_cpu = time_cpu;
+    p->arrival = arrival;
+
+    for (int i = 0; i <= 5; i++) {
+        p->time_io[i] = time_io[i];
+    }
+}
+
+//Gera 5 processos com valores definidos e retorna uma array de ponteiros de processos
+Process** generate_processes() {
+    Process* p1;
+    Process* p2;
+    Process* p3;
+    Process* p4;
+    Process* p5;
+    Process* process_list[5];
+
+    p1 = (Process*) malloc(sizeof(Process));
+    p2 = (Process*) malloc(sizeof(Process));
+    p3 = (Process*) malloc(sizeof(Process));
+    p4 = (Process*) malloc(sizeof(Process));
+    p5 = (Process*) malloc(sizeof(Process));
+    
+    init_process(p1, 1, 0, 3, 13, 0, NULL);
+    init_process(p2, 2, 0, 4, 11, 4, NULL);
+    init_process(p3, 3, 0, 1, 7, 5, NULL);
+    init_process(p4, 4, 0, 2, 8, 7, NULL);
+    init_process(p5, 5, 0, 5, 16, 10, NULL);
+
+    process_list[0] = p1;
+    process_list[1] = p2;
+    process_list[2] = p3;
+    process_list[3] = p4;
+    process_list[4] = p5;
+
+    return process_list;
+
+}
 
 int main(int argc, char **argv){
     /* TODO: funcao pra criar uma array de processos */
+    Process** p;
+    p = generate_processes();
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", (*(p + i))->pid);
+    }
+
     int t = 0;
     while(1){ // (SASSE)
 
